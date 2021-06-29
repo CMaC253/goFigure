3e0aac095c582bb882c60a2a3de9f5e7f0adc342
C Mac <colin_mcdonald2@msn.com>
2021-06-28 22:07:34 -0700

    tut 3

--- Hello/hello.go
+++ Hello/hello.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/cmac253/gofigure/greetings"
 )
 
 func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	//Get a greeting message and print it
-	message := greetings.Hello("Meryl")
+	message, err := greetings.Hello("Dookie")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,32 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 //returns greeting for named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Empty Name")
+	}
 	//return greeting that embeds name
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, v%. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+
 }
